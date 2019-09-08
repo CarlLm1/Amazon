64ef799899f34e4b2717b77d687d096c185461da
Carl Medina <carl.l.medina@comcast.net>
2019-09-07 22:08:08 -0700

    added functions to show the user cost when they select an item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,45 +4,65 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "Nov121990!",
     database: "bamazon_db"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    //console.log("connected as id " + connection.threadId + "\n");
     showProducts();
-  });
+});
 
-  function showProducts() {
+function showProducts() {
     console.log("Here are all of our amazing products:\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      userSearch ();
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        console.log(res);
+        userSearch();
     });
-  }
-  function userSearch(){
+}
+function userSearch() {
     inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "userItem",
-        message: "What is the ID number of the item you are buying today?"
-      },
-      {
-        type: "input",
-        name: "userQuantity",
-        message: "And how many would you like to buy today?"
-      }
-    ])
-  }
+        .prompt([
+            {
+                type: "input",
+                name: "userItem",
+                message: "What is the ID number of the item you are buying today?"
+            },
+            {
+                type: "input",
+                name: "userQuantity",
+                message: "And how many would you like to buy today?"
+            }
+        ])
+        .then(function (user) {
+            for (var i = 0; i < res.length; i++) {
+                if (user.userItem == res[i].id) {
+                    if (user.userQuantity > res[i].stock_quantity) {
+                        console.log("\nInsufficient quantity!")
+                        connection.end();
+                    } else {
+                        var cost = res[i].price * user.userQuantity
+
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [{ stock_quantity: newQuantity }, { id: id }],
+                            function (err) {
+                                if (err) throw err;
+                                console.log("\nThat'll be " + cost + "sir or ma'am!")
+                                connection.end();
+                            })
+                    }
+                }
+            }
+        })
+    }
