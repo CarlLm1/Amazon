d3c9495256c449ab72f6ec0734b663f843cab136
Carl Medina <carl.l.medina@comcast.net>
2019-09-07 20:21:58 -0700

    populated the table with 5 departments containing 2 products each

--- products.sql
+++ products.sql
@@ -5,10 +5,57 @@ USE bamazon_db;
 
 CREATE TABLE products
 (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR (50),
-    department_name VARCHAR (50),
-    price DECIMAL (10, 2),
-    stock_quantity INTEGER (10),
-    PRIMARY KEY (id)
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    product_name VARCHAR
+    (50),
+    department_name VARCHAR
+    (50),
+    price DECIMAL
+    (10, 2),
+    stock_quantity INTEGER
+    (10),
+    PRIMARY KEY
+    (id)
 );
+
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("nintendo switch", "electronics", 300, 30);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("laptop", "electronics", 1530, 15);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("hot cheetos", "food", 1.69, 9000);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("case of water", "food", 8, 8000);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("woody", "toys", 32, 300);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("buzz", "toys", 40, 1000);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("tuxedo", "clothing", 120, 12);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("bowtie", "clothing", 45, 45);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("listerine", "personal care", 5, 50000);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("loofah", "personal care", 2, 22222222);
